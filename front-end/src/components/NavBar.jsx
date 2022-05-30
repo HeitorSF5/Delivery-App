ffa57f13edb0fdb939bce7394a99553ae7322737
Heitor SF <heitor.sf5@gmail.com>
2022-05-30 19:17:53 -0300

    transfering front end files

---
+++ front-end/src/components/NavBar.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import NavBarButtons from './NavBarButtons';
+
+function NavBar({ pageName }) {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a
+            className="navbar-brand"
+            href="myProduct"
+            data-testid="customer_products__element-navbar-link-products"
+          >
+            {pageName}
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <NavBarButtons />
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
+
+NavBar.propTypes = {
+  pageName: PropTypes.string.isRequired,
+};
