af6c16fb7c35d281a46b46538e718dea4e65350b
Heitor SF <heitor.sf5@gmail.com>
2022-06-22 03:36:35 -0300

    streamlined pages so that there are no deadends

--- front-end/src/components/NavBarButtons.jsx
+++ front-end/src/components/NavBarButtons.jsx
@@ -10,7 +10,7 @@ export default function NavBarButtons() {
   return (
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
+        {/* <li className="nav-item">
           <a
             className="nav-link"
             aria-current="page"
@@ -19,7 +19,7 @@ export default function NavBarButtons() {
           >
             Orders
           </a>
-        </li>
+        </li> */}
 
         <li className="nav-item">
           <a
